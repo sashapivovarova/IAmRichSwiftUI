da7c0d0d36944343ef4aec1c06071ca2ad49b35e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-07-19 23:06:37 +0900

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Anastasia Grey on 2024/07/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Anastasia Grey on 2024/07/19.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
